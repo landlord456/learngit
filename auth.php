76a6fcd0a75f18e1bc898fe4b0b579eb218f8232
root <root@iZ94m4ug6e9Z.(none)>
2016-05-26 10:35:08 +0800

    add distributed

---
+++ auth.php
@@ -0,0 +1,33 @@
+<?php
+class Auth extends CI_Controller {
+	
+	function __construct(){
+		parent::__construct();
+	}
+	
+	public function token(){
+//		$url = $this->input->get('url');
+//		
+//		$this->smarty->assign('url', $url);
+		$this->smarty->display('login.html');
+	}
+	
+	public function check(){
+		session_start();
+		$this->load->library('redis');
+		$this->load->library('session');
+		
+		//$url = $this->input->post('url');
+		$token = $this->input->post('token');
+		$key = $this->redis->get('tmpkey');
+		
+		if($token == $key){
+			$this->session->set_userdata('token', 'yes');
+			echo "<script>window.location.href='index.php?controller=orders&method=check'</script>";
+		} else {
+			$this->smarty->assign('url', $url);
+			$this->smarty->display('login.html');			
+		}
+	}
+}
+?>
